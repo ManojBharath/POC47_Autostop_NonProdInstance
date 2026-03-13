9d311ab187bcb04ea2e8ba30973380e5f62b3b5d
ManojBharath <pmanojbharath@gmail.com>
2026-03-13 17:22:28 +0530

    update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ locals {
 # Create EC2 Instances using for_each
 resource "aws_instance" "servers" {
   for_each      = local.servers
-  ami           = "ami-02774d409be696d81"   # Amazon Linux 2 AMI (replace with your region's AMI)
+  ami           = "ami-02774d409be696d81" # Amazon Linux 2 AMI (replace with your region's AMI)
   instance_type = "t3.micro"
   tags = {
     Name = each.value.name
